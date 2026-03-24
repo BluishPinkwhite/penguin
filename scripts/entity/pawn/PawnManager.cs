65dafbb66c9e967f9355a1b01560dfaa36b6b392
Michal Kulhanek <misakulh@gmail.com>
2026-03-24 23:24:01 +0100

    Added main UI base

--- scripts/entity/pawn/PawnManager.cs
+++ scripts/entity/pawn/PawnManager.cs
@@ -10,10 +10,12 @@ public partial class PawnManager : Node2D
 {
     public override void _Ready()
     {
-        foreach (Role role in Enum.GetValues(typeof(Role)))
-        {
-            Inventory.Roles[role] = (int)(Game.RandomTo(2) + 2);
-        }
+        // foreach (Role role in Enum.GetValues(typeof(Role)))
+        // {
+            // Inventory.Roles[role] = (int)(Game.RandomTo(2) + 2);
+        // }
+        Inventory.Roles[Role.Miner] = 8;
+        Inventory.Roles[Role.Hauler] = 5;
 
         foreach (KeyValuePair<Role, int> pair in Inventory.Roles)
         {
