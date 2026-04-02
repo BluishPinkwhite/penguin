7623b5e74960ac3368bf195e308bcb23550e9668
Michal Kulhanek <misakulh@gmail.com>
2026-04-02 13:25:42 +0200

    Made the showcase kinda ready

--- scripts/ui/Clickable.cs
+++ scripts/ui/Clickable.cs
@@ -1,41 +1,62 @@
 using Godot;
 using System;
+using Incremental.scripts.director;
+using Incremental.scripts.entity.pawn;
+using Incremental.scripts.entity.pawn.roles;
 
 public partial class Clickable : Node
 {
-	private bool _isFocused;
-	
-	public void OnMouseEnter()
-	{
-		GD.Print("hover_enter " + Name);
-		_isFocused = true;
-	}
-	
-	public void OnMouseExit()
-	{
-		GD.Print("hover_exit " + Name);
-		_isFocused = false;
-	}
+    private bool _isFocused;
 
-	public override void _Input(InputEvent @event)
-	{
-		if (!_isFocused) return;
-		
-		base._Input(@event);
-		if (@event is InputEventMouseButton mouseEvent)
-		{
-			if (mouseEvent.ButtonIndex == MouseButton.Left && mouseEvent.Pressed)
-			{
-				OnClick();
-			}
-		}
-	}
+    [Export] private ClickableType _type;
+
+    public void OnMouseEnter()
+    {
+        // GD.Print("hover_enter " + Name);
+        _isFocused = true;
+    }
+
+    public void OnMouseExit()
+    {
+        // GD.Print("hover_exit " + Name);
+        _isFocused = false;
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (!_isFocused) return;
+
+        base._Input(@event);
+        if (@event is InputEventMouseButton mouseEvent)
+        {
+            if (mouseEvent.ButtonIndex == MouseButton.Left && mouseEvent.Pressed)
+            {
+                OnClick();
+            }
+        }
+    }
 
 
-	public void OnClick()
-	{
-		GD.Print("click " + Name);
-	}
-	
+    public void OnClick()
+    {
+        // GD.Print("click " + Name);
+        if (_type == ClickableType.MinerInc)
+        {
+            Inventory.Roles[Role.Miner]++;
+            Inventory.Items[Item.Dirt] -= 10;
+        }
+        else if (_type == ClickableType.CollectorInc)
+        {
+            Inventory.Roles[Role.Hauler]++;
+            Inventory.Items[Item.Dirt] -= 7;
+        }
+        Resources.I.UpdateVisuals();
+        
+    }
 }
 
+public enum ClickableType
+{
+    MinerInc,
+    CollectorInc
+}
